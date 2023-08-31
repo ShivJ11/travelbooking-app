eb4e0f8bbed4f6d3883802442b61e7aa8814f1ce
Naman Tripathi <namantripathi0532@gmail.com>
2023-08-31 11:49:22 +0530

    test change

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -27,6 +27,12 @@ const Navbar = () => {
             >
               Log In
             </a>
+             <a
+              href="#"
+              class="text-white bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:ring-black-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0"
+            >
+              Register
+            </a>
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
