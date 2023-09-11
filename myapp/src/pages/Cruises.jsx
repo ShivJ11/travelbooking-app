f3cd5a784663423465783885e6f54554f25bbc3a
Naman Tripathi <namantripathi0532@gmail.com>
2023-09-11 22:54:54 +0530

    Added Cruise Pages

--- myapp/src/pages/Cruises.jsx
+++ myapp/src/pages/Cruises.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+// import Header from "../components/Flight Page Component/Header";
+import CruiseHeader from "../components/Cruise Page Component/CruiseHeader";
+import { motion } from "framer-motion";
 
 const Cruises = () => {
   return (
-    <div>Cruises</div>
-  )
-}
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <CruiseHeader></CruiseHeader>
+    </motion.div>
+  );
+};
 
-export default Cruises
+export default Cruises;
