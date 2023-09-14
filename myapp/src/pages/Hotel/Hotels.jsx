e2798e83d7a6b4d9782a5b90db2c38388c4c7a3b
priyanshudabas <96956256+priyanshudabas@users.noreply.github.com>
2023-09-14 18:49:16 +0530

    API Consume

--- myapp/src/pages/Hotel/Hotels.jsx
+++ myapp/src/pages/Hotel/Hotels.jsx
@@ -152,7 +152,7 @@ const Hotels = () => {
           return (
             <div class="max-w-sm mb-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                <img class="rounded-t-lg" src={arr.hotelImageUrl} alt="" />
+                <img class="rounded-t-lg" src="https://www.desenchufateviajes.com.ar/buscarxpaquetes/assets/img/hotel-default.jpg" alt="" />
               </a>
               <div class="p-5">
                 <a href="#">
