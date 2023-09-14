1c93a06eed762b732332f88fdfc6510cf493ff05
Naman Tripathi <namantripathi0532@gmail.com>
2023-09-14 17:39:45 +0530

    Added search Bar in Hotel Page

--- myapp/src/pages/Hotel/Hotels.jsx
+++ myapp/src/pages/Hotel/Hotels.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Filter from "../../components/filter component/Filter";
 import "./hotel.css";
-
+import axios from "axios";
 const Hotels = () => {
+  const [hotels,setHotels] = useState([]);
+  const[hotelCity,setHotelCity] = useState("");
+  const onChangeHandler = (event) => {
+    setHotelCity(event.target.value );
+  };
+  const submitAction = async (e) => {
+    e.preventDefault();
+
+    const apiUrl = `http://localhost:5147/api/PublicData/filteredHotelDetails?city=${hotelCity}`;
+    try {
+      const result = await axios.get(apiUrl);
+      console.log(result);
+      setHotels(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="example-style">
       <div className="headerImage1 mb-6">
@@ -21,7 +38,67 @@ const Hotels = () => {
           </p>
         </div>
       </div>
-
+      
+<form onSubmit={submitAction} className="search-hotels">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input name="hotelCity" value={hotelCity} onChange={onChangeHandler} type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Hotels..." required/>
+        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+</form>
+<div className="hotels-data">
+{hotels.map((arr, index) => {
+          return (
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img class="rounded-t-lg" src={arr.hotelImageUrl} alt="" />
+              </a>
+              <div class="p-5">
+                <a href="#">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {arr.hotelName}
+                  </h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {arr.hotelDescription}
+                </p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Normal Room Price: {arr.normalRoomPrice}
+                </p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Deluxe Room Price : {arr.deluxeRoomPrice}
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Book Now
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+        </div>
       <div className="p-6" style={{ marginBottom: '200px' }}>
         <div className="float-left w-1/5">
           {/* <Filter></Filter> */}
